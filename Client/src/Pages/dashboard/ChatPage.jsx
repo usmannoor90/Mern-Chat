0744a024462f4a4d9388fee397a89a4a8c693daa
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2023-12-13 20:49:08 +0500

    more eye catching styling

--- Client/src/Pages/dashboard/ChatPage.jsx
+++ Client/src/Pages/dashboard/ChatPage.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import {
-  MultiChatSocket,
-  MultiChatWindow,
-  useMultiChatLogic,
-} from "react-chat-engine-advanced";
 
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 function ChatPage({ user }) {
-  const chatProps = useMultiChatLogic(
-    "6570553c-8817-4cbc-9b16-54cf820e17c3",
-    user.username,
-    user.secret
-  );
-
   return (
     <div style={{ height: "100vh" }}>
-      <MultiChatSocket {...chatProps} />
-      <PrettyChatWindow {...chatProps} style={{ height: "100%" }} />
+      <PrettyChatWindow
+        projectId="6570553c-8817-4cbc-9b16-54cf820e17c3"
+        username={user.username}
+        secret={user.secret}
+        style={{ height: "100vh" }}
+      />
     </div>
   );
 }
